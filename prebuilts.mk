SuperiorOS/android_vendor_prebuilts

prebuilts: Build Simple FOSS keyboard
Change-Id: Ifd661a2ed9ef928dc0b7f65cb7273db0b160f9a5
Signed-off-by: S I P U N <sipunkumar85@gmail.com>

prebuilts.mk
@@ -89,3 +89,9 @@ else
 PRODUCT_PACKAGES += \
     ViaBrowser
 endif
+
+# Keyboard
+ifneq ($(BUILD_WITH_GAPPS),true)
+PRODUCT_PACKAGES += \
+    Keyboard
+endif

ProjectInfinity-X/vendor_extras

prebuilts.mk
@@ -40,3 +40,9 @@ ifeq ($(TARGET_BUILD_VIMUSIC),true)
 PRODUCT_PACKAGES += \
     ViMusic
 endif
+
+# Keyboard
+ifneq ($(WITH_GAPPS),true)
+PRODUCT_PACKAGES += \
+    Keyboard
+endif