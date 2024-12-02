26ec4bc69b4774d46a30becafddd27058735eca4
tejas101k <tejassingh649@rediffmail.com>
2024-12-02 10:45:49 +0000

    extras: Drop SystemUI-NumOverlap Clockstyle

--- prebuilts.mk
+++ prebuilts.mk
@@ -43,7 +43,6 @@ PRODUCT_PACKAGES += \
     SystemUIClocks-Growth \
     SystemUIClocks-Inflate \
     SystemUIClocks-Metro \
-    SystemUIClocks-NumOverlap \
     SystemUIClocks-Weather
 
 # JudgeInterface
