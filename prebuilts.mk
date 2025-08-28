1e65c4673299ee31d66b009ae7936cc98e084f32
tejas101k <tejassingh649@rediffmail.com>
2025-08-28 20:48:58 +0000

    Extras: Guard Pixel sounds to Gapps only

--- prebuilts.mk
+++ prebuilts.mk
@@ -34,6 +34,7 @@ PRODUCT_PACKAGES += \
     MlkitBarcodeUIPrebuilt \
     VisionBarcodePrebuilt
 
+ifeq ($(WITH_GAPPS),true)
 # Sounds
 PRODUCT_COPY_FILES += \
     vendor/extras/sounds/Your_new_adventure.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/ringtones/Your_new_adventure.ogg \
@@ -44,6 +45,7 @@ PRODUCT_PRODUCT_PROPERTIES += \
     ro.config.ringtone=Your_new_adventure.ogg \
     ro.config.notification_sound=Eureka.ogg \
     ro.config.alarm_alert=Fresh_start.ogg
+endif
 
 # Pif
 include vendor/extras/pif/pif.mk
