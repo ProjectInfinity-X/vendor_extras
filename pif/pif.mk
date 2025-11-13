781ef1c4b42ab416ac7d7f93e97645f56a880898
Pranav Vashi <neobuddy89@gmail.com>
2025-11-13 17:27:24 +0000

    Extras: Update pif from mustang BP41.250916.010.A1

--- pif/pif.mk
+++ pif/pif.mk
@@ -5,9 +5,9 @@ PRODUCT_PRODUCT_PROPERTIES += \
     persist.sys.pihooks_mainline_PRODUCT?=caiman \
     persist.sys.pihooks_BRAND?=google \
     persist.sys.pihooks_MANUFACTURER?=Google \
-    persist.sys.pihooks_DEVICE?=comet \
-    persist.sys.pihooks_PRODUCT?=comet_beta \
-    persist.sys.pihooks_DEVICE_INITIAL_SDK_INT?=21 \
+    persist.sys.pihooks_DEVICE?=mustang \
+    persist.sys.pihooks_PRODUCT?=mustang_beta \
+    persist.sys.pihooks_DEVICE_INITIAL_SDK_INT?=32 \
     persist.sys.pihooks_SECURITY_PATCH?=2025-10-05 \
     persist.sys.pihooks_ID?=BP41.250916.010.A1
 
@@ -16,7 +16,7 @@ PRODUCT_PRODUCT_PROPERTIES += \
     remote_provisioning.hostname=remoteprovisioning.googleapis.com
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PihooksGmsFp="google/comet_beta/comet:16/BP41.250916.010.A1/14281945:user/release-keys" \
-    PihooksGmsModel="Pixel 9 Pro Fold" \
+    PihooksGmsFp="google/mustang_beta/mustang:16/BP41.250916.010.A1/14281945:user/release-keys" \
+    PihooksGmsModel="Pixel 10 Pro XL" \
     PihooksBuildFp="google/caiman/caiman:14/AD1A.240530.047.U1/12150698:user/release-keys" \
     PihooksBuildModel="Pixel 9 Pro"
